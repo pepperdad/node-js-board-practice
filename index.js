b1660571338d8a3d5b5e3f5c4a131000918940ed
pepperdad <jeong20909@naver.com>
2022-01-06 00:09:00 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser'); // 알아보기
+const mongoose = require('mongoose');
+
+// DB setting
+mongoose
+  .connect(
+    'mongodb+srv://jeong20909:aa^^ehdud12!@board.zpmr1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((e) => console.log('MongoDB error: ', e));
+
+// other setting
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Routes
+app.use('/', require('./routes/home'));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('server on! http://localhost:' + port);
+});
